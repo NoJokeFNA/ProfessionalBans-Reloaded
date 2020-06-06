b76428fed48725ba6a3fdc524d4528a60da56046
Julian <53055251+NoJokeFNA@users.noreply.github.com>
2020-06-06 07:37:44 +0200

    Improved code

--- src/de/tutorialwork/professionalbans/utils/UUIDFetcher.java
+++ src/de/tutorialwork/professionalbans/utils/UUIDFetcher.java
@@ -3,46 +3,49 @@ package de.tutorialwork.professionalbans.utils;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 
-public class UUIDFetcher
-{
-    private static HashMap<String, String> uuidCache = new HashMap();
+public class UUIDFetcher {
 
-    public static String getUUID(String username)
-    {
-        if (uuidCache.containsKey(username)) {
-            return (String)uuidCache.get(username);
-        }
-        try
-        {
-            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
+    private static final Map<String, String> UUID_CACHE = new HashMap();
+
+    public static String getUUID( String username ) {
+        if ( UUID_CACHE.containsKey( username ) )
+            return UUID_CACHE.get( username );
+
+        try {
+            URL url = new URL( "https://api.mojang.com/users/profiles/minecraft/" + username );
             InputStream stream = url.openStream();
-            InputStreamReader inr = new InputStreamReader(stream);
-            BufferedReader reader = new BufferedReader(inr);
-            String s = null;
-            StringBuilder sb = new StringBuilder();
-            while ((s = reader.readLine()) != null) {
-                sb.append(s);
-            }
-            String result = sb.toString();
-            JsonElement element = new JsonParser().parse(result);
-            JsonObject obj = element.getAsJsonObject();
-            String api = obj.get("id").toString();
-            api = api.substring(1);
-            api = api.substring(0, api.length() - 1);
-            StringBuffer sbu = new StringBuffer(api);
-            sbu.insert(8, "-").insert(13, "-").insert(18, "-").insert(23, "-");
-            String uuid = sbu.toString();
-            uuidCache.put(username, uuid);
+            InputStreamReader streamReader = new InputStreamReader( stream );
+            BufferedReader reader = new BufferedReader( streamReader );
+
+            String line;
+            StringBuilder builder = new StringBuilder();
+            while ( ( line = reader.readLine() ) != null ) builder.append( line );
+
+            String result = builder.toString();
+            JsonElement jsonElement = new JsonParser().parse( result );
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            String id = jsonObject.get( "id" ).toString();
+
+            id = id.substring( 1 );
+            id = id.substring( 0, id.length() - 1 );
+
+            StringBuilder stringBuilder = new StringBuilder( id );
+            stringBuilder.insert( 8, "-" ).insert( 13, "-" ).insert( 18, "-" ).insert( 23, "-" );
+            String uuid = stringBuilder.toString();
+
+            UUID_CACHE.put( username, uuid );
             return uuid;
+        } catch ( IOException | IllegalStateException ignored ) {
         }
-        catch (IOException|IllegalStateException localIOException) {}
         return null;
     }
 }
